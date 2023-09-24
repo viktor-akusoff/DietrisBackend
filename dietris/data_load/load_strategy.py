d9a0df59369e29ae7874e55051ebb3d756799127
viktor-akusoff <kirill.voloshin.98@ya.ru>
2023-09-25 01:06:52 +0300

    implemented xlrd strategy

--- dietris/data_load/load_strategy.py
+++ dietris/data_load/load_strategy.py
@@ -1,9 +1,11 @@
-from abc import ABCMeta, abstractmethod, abstractproperty
-from typing import List, Dict, LiteralString
+from abc import ABC, abstractmethod
+from typing import List, Dict, Union
+import xlrd # type: ignore
 
-NutritionTable = List[Dict[str, int]]
+NutritionData = Dict[str, Union[str, int]]
+NutritionTable = List[NutritionData]
 
-class Strategy(ABCMeta):
+class Strategy(ABC):
     
     table: NutritionTable = []
     
@@ -14,7 +16,18 @@ class Strategy(ABCMeta):
 class XLRDStrategy(Strategy):
     
     def load(self, file_address: str) -> None:
-        pass
+        book = xlrd.open_workbook(file_address)
+        first_sheet = book.sheet_by_index(0)
+        for i in range(1,first_sheet.nrows):
+            self.table.append(
+                {
+                    'name': first_sheet.cell_value(rowx=i, colx=0),
+                    'protein': first_sheet.cell_value(rowx=i, colx=1),
+                    'fats': first_sheet.cell_value(rowx=i, colx=2),
+                    'carb': first_sheet.cell_value(rowx=i, colx=3),
+                    'callories': first_sheet.cell_value(rowx=i, colx=4)
+                }                  
+            )
     
 class LightXLStrategy(Strategy):
     
